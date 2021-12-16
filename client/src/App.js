4701484cd577c209e93b94ec896e262c35ae4840
Daniel Hyman <dahyman91@gmail.com>
2021-12-16 13:47:39 -0500

    routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,20 @@
+import "./Pages/signup";
+import { Routes, Route } from "react-router-dom";
+import LogIn from "./Pages/login";
+import SignUp from "./Pages/signup";
+import Home from "./Pages/home";
 import "./App.css";
 
 function App() {
-  return <div className="App">Hi</div>;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/log-in" element={<LogIn />} />
+        <Route path="/home" element={<Home />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
